9f107c497d13092fd35e4ac20df3326e3d7cb164
romanyuvelir <romanshelukhov@gmail.com>
2025-02-19 19:00:15 +0300

    updated userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,10 +18,10 @@ module.exports.register = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await User.create({ email, username, password: hashedPassword });
 
-    const payload = { username: newUser.username, email: newUser.email, role: 'user' };
+    const payload = { id: newUser.id, username: newUser.username, email: newUser.email, role: 'user' };
     const token = sign(payload);
 
-    res.status(200).json({ message: 'User registered', token });
+    res.status(200).json({ message: 'User registered', id: newUser.id, token });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server Error' });
